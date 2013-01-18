f7d37d2b6568b087e5e48c2927343330d699d84c
John Mettraux <jmettraux@gmail.com>
2013-01-18 15:50:18 +0900

    set foundations for interpreter

---
+++ Intrigue.Tests/InterpreterTest.vb
@@ -0,0 +1,10 @@
+﻿
+Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+
+<TestClass()> Public Class InterpreterTest
+
+    <TestMethod()> Public Sub Interpreter_xxx()
+    End Sub
+End Class
