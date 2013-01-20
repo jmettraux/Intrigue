c2677de1652dbf8a065f9d273ddb255d69e41aec
John Mettraux <jmettraux@gmail.com>
2013-01-20 22:11:28 +0900

    split InterpreterTest

---
+++ Intrigue.Tests/FunctionTest.vb
@@ -0,0 +1,37 @@
+﻿
+Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+
+<TestClass()> Public Class FunctionTest
+
+    <TestMethod()> Public Sub Function_quote()
+
+        Assert.AreEqual(
+            "(1 2 3)",
+            Interpreter.DoEval("(quote 1 2 3)").ToString)
+        Assert.AreEqual(
+            "(1 2 3)",
+            Interpreter.DoEval("'(1 2 3)").ToString)
+    End Sub
+
+    <TestMethod()> Public Sub Function_car()
+
+        Assert.AreEqual(
+            "1",
+            Interpreter.DoEval("car '(1 2 3)").ToString)
+        Assert.AreEqual(
+            "1",
+            Interpreter.DoEval("(car (quote 1 2 3))").ToString)
+    End Sub
+
+    <TestMethod()> Public Sub Function_cdr()
+
+        Assert.AreEqual(
+            "(2 3)",
+            Interpreter.DoEval("cdr '(1 2 3)").ToString)
+        Assert.AreEqual(
+            "(2 3)",
+            Interpreter.DoEval("(cdr (quote 1 2 3))").ToString)
+    End Sub
+End Class
--- Intrigue.Tests/InterpreterTest.vb
+++ Intrigue.Tests/InterpreterTest.vb
@@ -21,34 +21,4 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
             """hell o""",
             Interpreter.DoEval("""hell o""").ToString)
     End Sub
-
-    <TestMethod()> Public Sub Interpreter_eval_quoted_list()
-
-        Assert.AreEqual(
-            "(1 2 3)",
-            Interpreter.DoEval("(quote 1 2 3)").ToString)
-        Assert.AreEqual(
-            "(1 2 3)",
-            Interpreter.DoEval("'(1 2 3)").ToString)
-    End Sub
-
-    <TestMethod()> Public Sub Interpreter_eval_car()
-
-        Assert.AreEqual(
-            "1",
-            Interpreter.DoEval("car '(1 2 3)").ToString)
-        Assert.AreEqual(
-            "1",
-            Interpreter.DoEval("(car (quote 1 2 3))").ToString)
-    End Sub
-
-    <TestMethod()> Public Sub Interpreter_eval_cdr()
-
-        Assert.AreEqual(
-            "(2 3)",
-            Interpreter.DoEval("cdr '(1 2 3)").ToString)
-        Assert.AreEqual(
-            "(2 3)",
-            Interpreter.DoEval("(cdr (quote 1 2 3))").ToString)
-    End Sub
 End Class
